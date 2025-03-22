8dd7a80b73cdb6ea62556023c8e80665af27d966
Facundo <facundolotobattan@gmail.com>
2025-03-22 15:35:28 -0300

    Refactor web scraping logic to improve product data extraction and include discount information. I can get the products´features for the first page.

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from curl_cffi import requests
 from lxml import html
-from lxml import etree
 import pandas as pd
 
 URL = "https://www.mercadolibre.com.ar/ofertas?container_id=MLA916439-1&page=1"
@@ -8,19 +7,22 @@ URL = "https://www.mercadolibre.com.ar/ofertas?container_id=MLA916439-1&page=1"
 
 response = requests.get(URL,impersonate="chrome110")
 tree = html.fromstring(response.text) #Ultra importante para usar Xpath
-products = tree.xpath('//*[@id="root-app"]/div/section/div[2]/div/div')
+products = tree.xpath('//div[contains(@class, "andes-card") and contains(@class, "poly-card")]')
 
 data = []
 for product in products:
-    current_price = product.xpath('//*[@id=":Ra1j7:"]/div[2]/div[2]/div/span[1]')
-    old_price = product.xpath('//*[@id=":Ra1j7:"]/div[2]/div[2]/s/span[2]')
-    seller = product.xpath('//*[@id=":Ra1j7:"]/div[2]/span[1]')
-    name = product.xpath('//*[@id=":Ra1j7:"]/div[2]/a/text()')
+    name = product.xpath('.//a[@class="poly-component__title"]/text()')
+    current_price = product.xpath('.//span[@class="andes-money-amount__fraction"]/text()')
+    old_price = product.xpath('.//s/span[2]/text()')
+    seller = product.xpath('.//span[contains(@class, "poly-component__seller")]/text()')
+    discount = product.xpath('.//span[contains(@class, "andes-money-amount__discount")]/text()')
 
-    data.append({'product':name,
-                 'seller':seller,
-                 'price':current_price,
-                 'old_price':old_price})
+    data.append({
+        'product': name[0] if name else None,
+        'seller': seller[0] if seller else None,
+        'price': current_price[0] if current_price else None,
+        'old_price': old_price[0] if old_price else None,
+        'discount': discount[0] if discount else None
+    })
 
 print(data)
-pass
